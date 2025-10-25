453352787de49258192b70da95a9d8148e719506
Uday Bhardwaj <142799565+uday-bhardwaj-15@users.noreply.github.com>
2025-10-25 10:00:35 +0530

    fixed build error

--- sanity.config.simple.ts
+++ sanity.config.simple.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'sanity'
-import { schemaTypes } from './sanity/schemas'
+// import { defineConfig } from 'sanity'
+// import { schemaTypes } from './sanity/schemas'
 
-export default defineConfig({
-  name: 'sensokart',
-  title: 'Sensokart CMS',
-  projectId: 'your-project-id',
-  dataset: 'production',
-  schema: {
-    types: schemaTypes,
-  },
-})
+// export default defineConfig({
+//   name: 'sensokart',
+//   title: 'Sensokart CMS',
+//   projectId: 'your-project-id',
+//   dataset: 'production',
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
-'use client'
-import { defineConfig } from 'sanity'
-import { schemaTypes } from './sanity/schemas'
+// 'use client'
+// import { defineConfig } from 'sanity'
+// import { schemaTypes } from './sanity/schemas'
 
-export default defineConfig({
-  name: 'sensokart',
-  title: 'Sensokart CMS',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'sh8xjsoh',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  plugins: [],
-  schema: {
-    types: schemaTypes,
-  },
-})
+// export default defineConfig({
+//   name: 'sensokart',
+//   title: 'Sensokart CMS',
+//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'sh8xjsoh',
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+//   plugins: [],
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
